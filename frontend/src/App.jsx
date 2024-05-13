4efca92276e1855f9fbdf3ec1c791d42e4933266
shubham30102002 <shubhamrg30@gmail.com>
2024-05-13 10:50:52 +0530

    Auth functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import {Toaster} from "react-hot-toast"
+
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup'
 import Home from './pages/home/home'
+import { useAuthContext } from './context/AuthContext'
 
 function App() {
+
+  const {authUser} = useAuthContext();
 
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        {/* <Login/> */}
-        {/* <Signup /> */}
-        <Home />
+        
+        <Routes >
+          <Route path='/' element={authUser ? <Home/> : <Navigate to={"/login"} /> } />
+          <Route path='/login' element={authUser? <Navigate to='/'/> : <Login/>} />
+          <Route path='/signup' element={authUser? <Navigate to='/'/> : <Signup/>} />
+
+        </Routes>
+
+        <Toaster />
       </div>
     </>
   )
